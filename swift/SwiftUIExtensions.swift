7000890c41c70d50dad21032a47e66921c02086f
Sam Pettersson <sam@sampettersson.com>
2021-12-14 18:39:22 +0100

    Add onTap tracker

--- swift/SwiftUIExtensions.swift
+++ swift/SwiftUIExtensions.swift
@@ -2,9 +2,20 @@ import Foundation
 import SwiftUI
 
 extension View {
-    public func trackView(_ track: AnalyticsClosure) -> some View {
+    /// sends a tracking event onAppear
+    public func trackOnAppear(_ track: AnalyticsClosure) -> some View {
         self.onAppear {
             track.send()
         }
+    }
+
+    /// sends a tracking event when tapping
+    public func trackOnTap(_ track: AnalyticsClosure) -> some View {
+        self.simultaneousGesture(
+            TapGesture()
+                .onEnded { _ in
+                    track.send()
+                }
+        )
     }
 }
