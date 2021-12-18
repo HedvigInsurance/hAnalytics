70fbdfc10713849d75ecd5621713f375476d39cb
Sam Pettersson <sam@sampettersson.com>
2021-12-18 13:37:07 +0100

    Compile

--- swift/hAnalytics.swift
+++ swift/hAnalytics.swift
@@ -103,9 +103,8 @@ extension hAnalyticsEvent {
 
         if let data = data {
           graphqlProperties = [
-            "TYPE_OF_CONTRACTS": (try? JMESExpression.compile("quotes.typeOfContract")).search(
-              object: data
-            )
+            "TYPE_OF_CONTRACTS": try? (try? JMESExpression.compile("quotes.typeOfContract"))?
+              .search(object: data)
           ]
         }
         else {
