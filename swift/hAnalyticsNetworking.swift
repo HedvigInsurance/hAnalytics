addd662fff49a33d1f74fbae2827f1878287a88e
Sam Pettersson <sam@sampettersson.com>
2022-01-25 11:39:02 +0100

    Make hAnalyticsNetworking public

--- swift/hAnalyticsNetworking.swift
+++ swift/hAnalyticsNetworking.swift
@@ -1,9 +1,9 @@
 import Foundation
 
-struct hAnalyticsNetworking {
-  static var httpAdditionalHeaders: [AnyHashable: Any] = [:]
-  static var trackingId: String = ""
-  static var endpointURL: String = ""
+public struct hAnalyticsNetworking {
+  public static var httpAdditionalHeaders: [AnyHashable: Any] = [:]
+  public static var trackingId: String = ""
+  public static var endpointURL: String = ""
 
   static func send(_ event: hAnalyticsEvent) {
     var urlRequest = URLRequest(url: URL(string: endpointURL)!)
