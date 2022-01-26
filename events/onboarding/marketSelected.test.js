cdd8b24ce24a44b3ec103f4227890f00854af12f
Sam Pettersson <sam@sampettersson.com>
2022-01-26 10:02:18 +0100

    Add market selected event

---
+++ events/onboarding/marketSelected.test.js
@@ -0,0 +1,9 @@
+const validateEvent = require("../../commons/validateEvent.js")
+
+test('validate', () => {
+    const event = validateEvent(__filename, {
+        locale: "en-DK"
+    })
+
+    expect(event).toMatchSnapshot()
+});
