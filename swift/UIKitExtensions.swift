4823414c7d146576901bd78303f959f0970acf92
Sam Pettersson <sam@sampettersson.com>
2022-01-26 10:35:54 +0100

    Add rootView label

--- swift/UIKitExtensions.swift
+++ swift/UIKitExtensions.swift
@@ -5,6 +5,6 @@ import SwiftUI
 extension UIViewController {
     /// Adds a SwiftUI view to the hierarchy that sends a tracking view on appear for UIViewController
     public func trackOnAppear(_ parcel: hAnalyticsParcel) {
-        self.view.addSubview(HostingView(Color.clear.trackOnAppear(parcel)))
+        self.view.addSubview(HostingView(rootView: Color.clear.trackOnAppear(parcel)))
     } 
 }
