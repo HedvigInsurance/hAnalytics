4d27b4db8a043b74eaf16e2cfa084c640d27f284
Sam Pettersson <sam@sampettersson.com>
2022-01-26 10:41:41 +0100

    Add some more UIKit extensions

--- docs/docs/swift/UIKit.md
+++ docs/docs/swift/UIKit.md
@@ -5,3 +5,6 @@ hAnalytics defines a set of extensions to UIKIT to make tracking easier
 
 ## UIViewController.trackOnAppear
 Dispatches an event each time a view added to the UIViewController hierarchy appears, useful to track screen views and similar.
+
+## UIView.trackOnAppear
+Dispatches an event each time a view added to the UIView hierarchy appears, useful to track screen views and similar.
--- swift/UIKitExtensions.swift
+++ swift/UIKitExtensions.swift
@@ -8,3 +8,10 @@ extension UIViewController {
         self.view.addSubview(HostingView(rootView: Color.clear.trackOnAppear(parcel)))
     } 
 }
+
+extension UIView {
+    /// Adds a SwiftUI view to the hierarchy that sends a tracking view on appear for UIView
+    public func trackOnAppear(_ parcel: hAnalyticsParcel) {
+        self.view.addSubview(HostingView(rootView: Color.clear.trackOnAppear(parcel)))
+    } 
+}
