7dbf57699cda7dda318ecc227451dd3e38740a10
Sam Pettersson <sam@sampettersson.com>
2022-01-26 11:00:16 +0100

    Fix UIView trackOnAppear

--- swift/UIKitExtensions.swift
+++ swift/UIKitExtensions.swift
@@ -12,6 +12,6 @@ extension UIViewController {
 extension UIView {
     /// Adds a SwiftUI view to the hierarchy that sends a tracking view on appear for UIView
     public func trackOnAppear(_ parcel: hAnalyticsParcel) {
-        self.view.addSubview(HostingView(rootView: Color.clear.trackOnAppear(parcel)))
+        self.addSubview(HostingView(rootView: Color.clear.trackOnAppear(parcel)))
     } 
 }
