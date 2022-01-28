a29b1ecec239a90e697188201c8403185ef3e4f5
Sam Pettersson <sam@sampettersson.com>
2022-01-28 15:57:25 +0100

    Make Kotlin type map handle types better

--- commons/typeMaps.js
+++ commons/typeMaps.js
@@ -25,25 +25,33 @@ const getSwiftType = (type) => {
     );
 }
 
-module.exports = {
-    swiftTypeMap: getSwiftType,
-    kotlinTypeMap: {
+const getKotlinType = (type) => {
+    const primitives = {
         "String": "String",
         "Integer": "Int",
         "Boolean": "Boolean",
         "Double": "Double",
-        "Optional<String>": "String?",
-        "Optional<Integer>": "Int?",
-        "Optional<Boolean>": "Boolean?",
-        "Optional<Double>": "Double?",
-        "Array<String>": "Array<String>",
-        "Array<Integer>": "Array<Int>",
-        "Array<Boolean>": "Array<Boolean>",
-        "Array<Double>": "Array<Double>",
-        "Array<Optional<String>>": "Array<String?>",
-        "Array<Optional<Integer>>": "Array<Int?>",
-        "Array<Optional<Boolean>>": "Array<Boolean?>",
-        "Array<Optional<Double>>": "Array<Double?>",
-        "Dictionary<String, Any>": "Map<String, Any>"
-    },
+        "Optional": (s) => `${s}?`,
+        "Array": (s) => `Array<${s}>`,
+        "Dictionary": (s) => `Map<${s}>`
+    }
+
+    return type.split("<").reverse().reduce(
+        (acc, curr) => {
+            const currWithoutBrackets = curr.replaceAll(">", "")
+            const primitive = primitives[currWithoutBrackets]
+
+            if (typeof primitive === 'function') {
+                return primitive(acc ?? "")
+            }
+
+            return primitive ? primitive : currWithoutBrackets
+        },
+        ""
+    );
+}
+
+module.exports = {
+    swiftTypeMap: getSwiftType,
+    kotlinTypeMap: getKotlinType
 }
--- commons/typeMaps.test.js
+++ commons/typeMaps.test.js
@@ -4,4 +4,12 @@ test('swiftTypeMap', () => {
     expect(typeMaps.swiftTypeMap("Array<String>")).toEqual("[String]")
     expect(typeMaps.swiftTypeMap("Array<Boolean>")).toEqual("[Bool]")
     expect(typeMaps.swiftTypeMap("Array<Dictionary<String, String>>")).toEqual("[[String: String]]")
+    expect(typeMaps.kotlinTypeMap("Optional<String>")).toEqual("String?")
+});
+
+test('kotlinTypeMap', () => {
+    expect(typeMaps.kotlinTypeMap("Array<String>")).toEqual("Array<String>")
+    expect(typeMaps.kotlinTypeMap("Array<Boolean>")).toEqual("Array<Boolean>")
+    expect(typeMaps.kotlinTypeMap("Array<Dictionary<String, String>>")).toEqual("Array<Map<String, String>>")
+    expect(typeMaps.kotlinTypeMap("Optional<String>")).toEqual("String?")
 });
