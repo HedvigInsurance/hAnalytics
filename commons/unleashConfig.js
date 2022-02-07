35630fe37f0aac25541a6f4ebd5d33db022f3520
Sam Pettersson <sam@sampettersson.com>
2022-02-07 13:10:42 +0100

    Use redis cache if available

--- commons/unleashConfig.js
+++ commons/unleashConfig.js
@@ -1,21 +1,46 @@
-const { Strategy } = require("unleash-client");
+const { Strategy, InMemStorageProvider } = require("unleash-client");
+
+const { createClient } = require("redis");
+
+class RedisStorageProvider {
+  client = null;
+
+  constructor() {
+    this.client = createClient({
+      url: process.env.REDIS,
+    });
+    this.client.connect();
+  }
+
+  async set(key, data) {
+    await this.client.set(key, JSON.stringify(data));
+  }
+
+  async get(key) {
+    const data = await this.client.get(key);
+    return JSON.parse(data);
+  }
+}
 
 class MemberIdsStrategy extends Strategy {
-    constructor() {
-        super("MemberIds");
-    }
+  constructor() {
+    super("MemberIds");
+  }
 
-    isEnabled(parameters, context) {
-        return parameters.memberIds.includes(context.memberId);
-    }
+  isEnabled(parameters, context) {
+    return parameters.memberIds.includes(context.memberId);
+  }
 }
 
 module.exports = {
-    appName: 'hanalytics',
-    url: process.env.UNLEASH_API_URL,
-    customHeaders: {
-      Authorization: process.env.UNLEASH_API_KEY,
-    },
-    environment: process.env.UNLEASH_API_KEY.replace("*:").split(".")[0],
-    strategies: [new MemberIdsStrategy()],
-}
+  appName: "hanalytics",
+  url: process.env.UNLEASH_API_URL,
+  customHeaders: {
+    Authorization: process.env.UNLEASH_API_KEY,
+  },
+  environment: process.env.UNLEASH_API_KEY.replace("*:").split(".")[0],
+  strategies: [new MemberIdsStrategy()],
+  storageProvider: process.env.REDIS
+    ? new RedisStorageProvider()
+    : new InMemStorageProvider(),
+};
