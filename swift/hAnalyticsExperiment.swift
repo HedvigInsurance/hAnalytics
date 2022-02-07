136e268ee78202f6cb0017094b7766bda19fdce4
sampettersson <sampettersson@users.noreply.github.com>
2022-02-07 10:51:07 +0000

    Commit from GitHub Actions (Compile and release)

--- swift/hAnalyticsExperiment.swift
+++ swift/hAnalyticsExperiment.swift
@@ -103,10 +103,10 @@ public struct hAnalyticsExperiment {
       return variant
     }
 
-    hAnalyticsEvent.experimentEvaluated(name: "payment_type", variant: PaymentType.adyen.rawValue)
+    hAnalyticsEvent.experimentEvaluated(name: "payment_type", variant: PaymentType.trustly.rawValue)
       .send()
 
-    return .adyen
+    return .trustly
   }
 
 }
