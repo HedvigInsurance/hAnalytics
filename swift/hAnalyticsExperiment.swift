317d6127fa973083f0f2735e2277bf6a6921aaaa
Sam Pettersson <sam@sampettersson.com>
2022-02-07 08:59:34 +0100

    Codegen

--- swift/hAnalyticsExperiment.swift
+++ swift/hAnalyticsExperiment.swift
@@ -1,176 +1,116 @@
 import Foundation
 
-
-    /// Which login method to use
-    public enum LoginMethod: String {
-       
-        case bankIdSweden = "bank_id_sweden"
-       
-        case disabled = "disabled"
-       
-        case otp = "otp"
-       
-        case simpleSign = "simple_sign"
-       
-       
-    }
-
-    /// Which payment provider to use
-    public enum PaymentType: String {
-       
-        case adyen = "adyen"
-       
-        case disabled = "disabled"
-       
-        case trustly = "trustly"
-       
-       
-    }
-
-
-public struct hAnalyticsExperiment {
-// loads all experiments from server
-public static func load(onComplete: @escaping (_ success: Bool) -> Void) {
-    hAnalyticsNetworking.loadExperiments(onComplete: onComplete)
+/// Which login method to use
+public enum LoginMethod: String {
+  case bankIdSweden = "bank_id_sweden"
+  case disabled = "disabled"
+  case otp = "otp"
+  case simpleSign = "simple_sign"
 }
 
+/// Which payment provider to use
+public enum PaymentType: String {
+  case adyen = "adyen"
+  case disabled = "disabled"
+  case trustly = "trustly"
+}
 
-    
-    /// Is the forever february campaign activated
-    public static var foreverFebruaryCampaign: Bool {
-       if let experiment = hAnalyticsNetworking.experimentsPayload.first(where: { experiment in
-            experiment["name"] == "forever_february_campaign"
-       }), let variant = experiment["variant"] {
-            hAnalyticsEvent.experimentEvaluated(
-                name: "forever_february_campaign",
-                variant: variant
-            ).send()
-           
-           return variant == "enabled"
-       }
+public struct hAnalyticsExperiment {
+  // loads all experiments from server
+  public static func load(onComplete: @escaping (_ success: Bool) -> Void) {
+    hAnalyticsNetworking.loadExperiments(onComplete: onComplete)
+  }
 
-       hAnalyticsEvent.experimentEvaluated(
-            name: "forever_february_campaign",
-            variant: "disabled"
-        ).send()
-
-       return false
+  /// Is the forever february campaign activated
+  public static var foreverFebruaryCampaign: Bool {
+    if let experiment = hAnalyticsNetworking.experimentsPayload.first(where: { experiment in
+      experiment["name"] == "forever_february_campaign"
+    }), let variant = experiment["variant"] {
+      hAnalyticsEvent.experimentEvaluated(name: "forever_february_campaign", variant: variant)
+        .send()
+      return variant == "enabled"
     }
-    
 
-    
-    /// Should the french market be shown
-    public static var frenchMarket: Bool {
-       if let experiment = hAnalyticsNetworking.experimentsPayload.first(where: { experiment in
-            experiment["name"] == "french_market"
-       }), let variant = experiment["variant"] {
-            hAnalyticsEvent.experimentEvaluated(
-                name: "french_market",
-                variant: variant
-            ).send()
-           
-           return variant == "enabled"
-       }
+    hAnalyticsEvent.experimentEvaluated(name: "forever_february_campaign", variant: "disabled")
+      .send()
 
-       hAnalyticsEvent.experimentEvaluated(
-            name: "french_market",
-            variant: "disabled"
-        ).send()
+    return false
+  }
 
-       return false
+  /// Should the french market be shown
+  public static var frenchMarket: Bool {
+    if let experiment = hAnalyticsNetworking.experimentsPayload.first(where: { experiment in
+      experiment["name"] == "french_market"
+    }), let variant = experiment["variant"] {
+      hAnalyticsEvent.experimentEvaluated(name: "french_market", variant: variant).send()
+      return variant == "enabled"
     }
-    
 
-    
-    /// Is the key gear feature activated
-    public static var keyGear: Bool {
-       if let experiment = hAnalyticsNetworking.experimentsPayload.first(where: { experiment in
-            experiment["name"] == "key_gear"
-       }), let variant = experiment["variant"] {
-            hAnalyticsEvent.experimentEvaluated(
-                name: "key_gear",
-                variant: variant
-            ).send()
-           
-           return variant == "enabled"
-       }
+    hAnalyticsEvent.experimentEvaluated(name: "french_market", variant: "disabled").send()
 
-       hAnalyticsEvent.experimentEvaluated(
-            name: "key_gear",
-            variant: "disabled"
-        ).send()
+    return false
+  }
 
-       return false
+  /// Is the key gear feature activated
+  public static var keyGear: Bool {
+    if let experiment = hAnalyticsNetworking.experimentsPayload.first(where: { experiment in
+      experiment["name"] == "key_gear"
+    }), let variant = experiment["variant"] {
+      hAnalyticsEvent.experimentEvaluated(name: "key_gear", variant: variant).send()
+      return variant == "enabled"
     }
-    
 
-    
-    /// Which login method to use
-    public static var loginMethod: LoginMethod {
-       if let experiment = hAnalyticsNetworking.experimentsPayload.first(where: { experiment in
-            experiment["name"] == "login_method"
-       }), let variant = LoginMethod(rawValue: experiment["variant"] ?? "") {
-           hAnalyticsEvent.experimentEvaluated(
-               name: "login_method",
-               variant: variant.rawValue
-            ).send()
-           
-           return variant
-       }
+    hAnalyticsEvent.experimentEvaluated(name: "key_gear", variant: "disabled").send()
 
-       hAnalyticsEvent.experimentEvaluated(
-            name: "login_method",
-            variant: LoginMethod.disabled.rawValue
-       ).send()
+    return false
+  }
 
-        return .disabled
+  /// Which login method to use
+  public static var loginMethod: LoginMethod {
+    if let experiment = hAnalyticsNetworking.experimentsPayload.first(where: { experiment in
+      experiment["name"] == "login_method"
+    }), let variant = LoginMethod(rawValue: experiment["variant"] ?? "") {
+      hAnalyticsEvent.experimentEvaluated(name: "login_method", variant: variant.rawValue).send()
+      return variant
     }
-    
 
-    
-    /// Is moving flow activated
-    public static var movingFlow: Bool {
-       if let experiment = hAnalyticsNetworking.experimentsPayload.first(where: { experiment in
-            experiment["name"] == "moving_flow"
-       }), let variant = experiment["variant"] {
-            hAnalyticsEvent.experimentEvaluated(
-                name: "moving_flow",
-                variant: variant
-            ).send()
-           
-           return variant == "enabled"
-       }
+    hAnalyticsEvent.experimentEvaluated(
+      name: "login_method",
+      variant: LoginMethod.disabled.rawValue
+    ).send()
 
-       hAnalyticsEvent.experimentEvaluated(
-            name: "moving_flow",
-            variant: "disabled"
-        ).send()
+    return .disabled
+  }
 
-       return false
+  /// Is moving flow activated
+  public static var movingFlow: Bool {
+    if let experiment = hAnalyticsNetworking.experimentsPayload.first(where: { experiment in
+      experiment["name"] == "moving_flow"
+    }), let variant = experiment["variant"] {
+      hAnalyticsEvent.experimentEvaluated(name: "moving_flow", variant: variant).send()
+      return variant == "enabled"
     }
-    
 
-    
-    /// Which payment provider to use
-    public static var paymentType: PaymentType {
-       if let experiment = hAnalyticsNetworking.experimentsPayload.first(where: { experiment in
-            experiment["name"] == "payment_type"
-       }), let variant = PaymentType(rawValue: experiment["variant"] ?? "") {
-           hAnalyticsEvent.experimentEvaluated(
-               name: "payment_type",
-               variant: variant.rawValue
-            ).send()
-           
-           return variant
-       }
+    hAnalyticsEvent.experimentEvaluated(name: "moving_flow", variant: "disabled").send()
 
-       hAnalyticsEvent.experimentEvaluated(
-            name: "payment_type",
-            variant: PaymentType.disabled.rawValue
-       ).send()
+    return false
+  }
 
-        return .disabled
+  /// Which payment provider to use
+  public static var paymentType: PaymentType {
+    if let experiment = hAnalyticsNetworking.experimentsPayload.first(where: { experiment in
+      experiment["name"] == "payment_type"
+    }), let variant = PaymentType(rawValue: experiment["variant"] ?? "") {
+      hAnalyticsEvent.experimentEvaluated(name: "payment_type", variant: variant.rawValue).send()
+      return variant
     }
-    
+
+    hAnalyticsEvent.experimentEvaluated(
+      name: "payment_type",
+      variant: PaymentType.disabled.rawValue
+    ).send()
+
+    return .disabled
+  }
 
 }
