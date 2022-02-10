f36d72121e975c1586f784139ba2b4aa02bba7bb
Sam Pettersson <sam@sampettersson.com>
2022-02-10 15:33:05 +0100

    Allow to pass in credentials through ENV

--- server/bigquery/config.js
+++ server/bigquery/config.js
@@ -1,5 +1,7 @@
 const { BigQuery } = require("@google-cloud/bigquery");
-const bigquery = new BigQuery();
+const bigquery = new BigQuery({
+    credentials: process.env.GOOGLE_CREDENTIALS ? JSON.parse(process.env.GOOGLE_CREDENTIALS) : undefined
+});
 
 const dataset = process.env.BQ_DATASET;
 
