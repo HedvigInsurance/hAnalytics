6ef8bbf29717ca1f90d45fff8db095e697582ffc
sampettersson <sampettersson@users.noreply.github.com>
2022-02-10 14:58:58 +0000

    Commit from GitHub Actions (Compile and release)

--- swift/hAnalyticsExperiment.swift
+++ swift/hAnalyticsExperiment.swift
@@ -95,10 +95,10 @@ public struct hAnalyticsExperiment {
       return variant
     }
 
-    hAnalyticsEvent.experimentEvaluated(name: "login_method", variant: LoginMethod.otp.rawValue)
+    hAnalyticsEvent.experimentEvaluated(name: "login_method", variant: LoginMethod.nemId.rawValue)
       .send()
 
-    return .otp
+    return .nemId
   }
 
   /// Is moving flow activated
@@ -124,10 +124,10 @@ public struct hAnalyticsExperiment {
       return variant
     }
 
-    hAnalyticsEvent.experimentEvaluated(name: "payment_type", variant: PaymentType.trustly.rawValue)
+    hAnalyticsEvent.experimentEvaluated(name: "payment_type", variant: PaymentType.adyen.rawValue)
       .send()
 
-    return .trustly
+    return .adyen
   }
 
   /// Show payment step in PostOnboarding
