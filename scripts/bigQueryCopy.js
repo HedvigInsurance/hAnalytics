28dff8c3f49eb638c623476224e12fccdc368667
Sam Pettersson <sam@sampettersson.com>
2022-02-15 20:11:36 +0100

    Remove log

--- scripts/bigQueryCopy.js
+++ scripts/bigQueryCopy.js
@@ -40,8 +40,6 @@ const upsert = async () => {
       (field) => field.name !== "loaded_at"
     );
 
-    console.log(fieldsExcludingLoadedAt);
-
     await setupTable(
       destination,
       event.description,
