c150f7f556608b6b8415b161972e6538820d0a0d
Sam Pettersson <sam@sampettersson.com>
2022-03-01 15:55:22 +0100

    Remove deprecated events

--- definitions/events/depreceated/screenViewCommonClaim.test.js
+++
@@ -1,9 +0,0 @@
-const validateEvent = require("commons/validateEvent.js");
-
-test("validate", () => {
-  const event = validateEvent(__filename, {
-    id: "the_id",
-  });
-
-  expect(event).toMatchSnapshot();
-});
