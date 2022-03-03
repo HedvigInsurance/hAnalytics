191eb33e312cac198321249da657ab02488106bd
sampettersson <sampettersson@users.noreply.github.com>
2022-03-03 10:48:24 +0000

    Commit from GitHub Actions (Compile and release)

--- client/ts/hAnalyticsExperiments.ts
+++ client/ts/hAnalyticsExperiments.ts
@@ -1,57 +1,62 @@
-import { hAnalyticsTrackers } from "./hAnalyticsTrackers"
-import { hAnalyticsExperiment } from "./hAnalyticsExperiment"
-import { hAnalyticsNetworking } from "./hAnalyticsNetworking"
-
-
+import { hAnalyticsTrackers } from "./hAnalyticsTrackers";
+import { hAnalyticsExperiment } from "./hAnalyticsExperiment";
+import { hAnalyticsNetworking } from "./hAnalyticsNetworking";
 
 export class hAnalyticsExperiments {
-    private trackers: hAnalyticsTrackers
-    private networking: hAnalyticsNetworking
-    loading: boolean = true
+  private trackers: hAnalyticsTrackers;
+  private networking: hAnalyticsNetworking;
+  loading: boolean = true;
 
-    constructor(
-        trackers: hAnalyticsTrackers,
-        networking: hAnalyticsNetworking,
-        bootstrapList?: hAnalyticsExperiment[]
-    ) {
-        this.trackers = trackers
-        this.networking = networking
-        this.networking.bootstrapExperiments(bootstrapList)
-        this.loading = bootstrapList ? false : true
+  constructor(
+    trackers: hAnalyticsTrackers,
+    networking: hAnalyticsNetworking,
+    bootstrapList?: hAnalyticsExperiment[]
+  ) {
+    this.trackers = trackers;
+    this.networking = networking;
+    this.networking.bootstrapExperiments(bootstrapList);
+    this.loading = bootstrapList ? false : true;
+  }
+
+  // loads all experiments from server
+  async load(): Promise<hAnalyticsExperiment[]> {
+    const list = await this.networking.loadExperiments([
+      "allow_external_data_collection",
+      "forever_february_campaign",
+      "french_market",
+      "key_gear",
+      "login_method",
+      "moving_flow",
+      "payment_type",
+      "post_onboarding_show_payment_step",
+      "update_necessary",
+      "use_hedvig_letters_font",
+    ]);
+    this.loading = false;
+    return list;
+  }
+
+  /// no description given
+  allowExternalDataCollection(): boolean {
+    const experiment = this.networking.findExperimentByName(
+      "allow_external_data_collection"
+    );
+    const variant = experiment.variant;
+
+    if (variant) {
+      this.trackers.experimentEvaluated(
+        "allow_external_data_collection",
+        variant
+      );
+
+      return variant == "enabled";
     }
 
-    // loads all experiments from server
-    async load(): Promise<hAnalyticsExperiment[]> {
-        const list = await this.networking.loadExperiments(
-            ["allow_external_data_collection","forever_february_campaign","french_market","key_gear","login_method","moving_flow","payment_type","post_onboarding_show_payment_step","update_necessary","use_hedvig_letters_font"]
-        )
-        this.loading = false
-        return list
-    }
+    this.trackers.experimentEvaluated(
+      "allow_external_data_collection",
+      "disabled"
+    );
 
-
-    
-    /// no description given
-    allowExternalDataCollection(): boolean {
-        const experiment = this.networking.findExperimentByName("allow_external_data_collection")
-        const variant = experiment.variant
-
-        if (variant) {
-            this.trackers.experimentEvaluated(
-               "allow_external_data_collection",
-               variant
-            )
-            
-            return variant == "enabled"
-        }
-
-        this.trackers.experimentEvaluated(
-            "allow_external_data_collection",
-            "disabled"
-        )
-
-        return false
-    }
-    
-
+    return false;
+  }
 }
